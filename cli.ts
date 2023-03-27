1b840547e61bb23349d437afb549a5769571b86b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-27 09:34:18 +0800

    add cli to create npm starter template

---
+++ cli.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+import { writeFileSync } from 'fs'
+import { copyTemplate, getDest } from 'npm-init-helper'
+import { join } from 'path'
+
+async function main() {
+  let dest = await getDest()
+  let srcDir = join(__dirname, 'template')
+  console.log('Copying rpc template to:', dest, '...')
+  await copyTemplate({ srcDir, dest, updatePackageJson: false })
+  let helpMessage = `
+Inside the server directory, you can run several commands:
+
+  npm start
+    Starts the auto-refresh development server.
+    It auto generates the client/src/sdk.ts based on the APIs defined in server/src/core.ts.
+
+  npm run db:setup
+    Migrate the database schema to latest version.
+
+  npm run db:plan
+    Auto-generate migration based on erd.txt and current database schema.
+
+  npm run db:update
+    Apply the new migration plan, and update the proxy.ts based on the erd.txt.
+
+  npm run build
+    Builds the web project into 'build' folder.
+
+
+Get started by typing:
+
+  cd ${dest}/server
+  pnpm i
+  npm run db:setup
+  npm start
+
+
+Installation Alternatives:
+
+  pnpm i
+  or
+  yarn install
+  or
+  npm install
+`
+  console.log(
+    `
+Done.
+
+Created ${dest}/server and ${dest}/client.
+
+${helpMessage.trim()}
+`.trim(),
+  )
+  writeFileSync(join(dest, 'help.txt'), helpMessage.trimStart())
+}
+
+main().catch(e => console.error(e))
