43cf9f32754f9c01fcee0325875f87475782d62b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-28 21:22:44 +0800

    enable cors in template server

--- template/server/src/app.ts
+++ template/server/src/app.ts
@@ -1,8 +1,10 @@
+import cors from 'cors'
 import express from 'express'
 import { core } from './core'
 
 export let app = express()
 
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
