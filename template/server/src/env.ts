08e2807f71c16ca6e04d855d50ea602ad524d60e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-04-08 17:52:05 +0800

    do not require JWT_SECRET in development mode

--- template/server/src/env.ts
+++ template/server/src/env.ts
@@ -4,9 +4,15 @@ import populateEnv from 'populate-env'
 config()
 
 export let env = {
+  NODE_ENV: 'development',
   ORIGIN: 'http://localhost:3000',
   PORT: 3000,
-  JWT_SECRET: '',
+  JWT_SECRET: ' ',
 }
 
 populateEnv(env, { mode: 'halt' })
+
+if (env.NODE_ENV === 'production' && env.JWT_SECRET === ' ') {
+  console.error('Error: missing JWT_SECRET in .env')
+  process.exit(1)
+}
