a1b9a48494b9453b204a675a012fa891174ab16b
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-14 22:45:13 +0800

    chore: add client upload demo to the restful template - taking reference on the upload demo in other template

---
+++ templates/restful/client/test/upload-demo.ts
@@ -0,0 +1,29 @@
+import { login } from '../src/api/user'
+import { uploadFiles } from '../src/api/upload-files'
+
+let input = document.createElement('input')
+input.type = 'file'
+input.multiple = true
+input.onchange = async () => {
+  if (!input.files) return
+  try {
+    await login({
+      body: {
+        username: 'alice',
+        password: 'secret',
+      },
+    })
+    let json = await uploadFiles(input.files)
+    console.log(json)
+    code.textContent = JSON.stringify(json, null, 2)
+  } catch (error) {
+    console.log(error)
+    code.textContent = String(error)
+  }
+}
+document.body.appendChild(input)
+
+let pre = document.createElement('pre')
+let code = document.createElement('code')
+pre.appendChild(code)
+document.body.appendChild(pre)
